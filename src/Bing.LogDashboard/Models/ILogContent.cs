b3ae67347f46e1bf685dabca384302f65ec3af55
jian玄冰 <jianxuanhuo1@126.com>
2019-07-03 18:35:53 +0800

    初始化项目

---
+++ src/Bing.LogDashboard/Models/ILogContent.cs
@@ -0,0 +1,158 @@
+﻿namespace Bing.LogDashboard.Models
+{
+    /// <summary>
+    /// 日志内容
+    /// </summary>
+    public interface ILogContent
+    {
+        /// <summary>
+        /// 系统编号
+        /// </summary>
+        int Id { get; set; }
+
+        /// <summary>
+        /// 日志名称
+        /// </summary>
+        string LogName { get; set; }
+
+        /// <summary>
+        /// 日志级别
+        /// </summary>
+        string Level { get; set; }
+
+        /// <summary>
+        /// 跟踪号
+        /// </summary>
+        string TraceId { get; set; }
+
+        /// <summary>
+        /// 操作时间
+        /// </summary>
+        string OperationTime { get; set; }
+
+        /// <summary>
+        /// 执行时间
+        /// </summary>
+        string Duration { get; set; }
+
+        /// <summary>
+        /// IP
+        /// </summary>
+        string Ip { get; set; }
+
+        /// <summary>
+        /// 主机
+        /// </summary>
+        string Host { get; set; }
+
+        /// <summary>
+        /// 线程号
+        /// </summary>
+        string ThreadId { get; set; }
+
+        /// <summary>
+        /// 浏览器
+        /// </summary>
+        string Browser { get; set; }
+
+        /// <summary>
+        /// 请求地址
+        /// </summary>
+        string Url { get; set; }
+
+        /// <summary>
+        /// 业务编号
+        /// </summary>
+        string BusinessId { get; set; }
+
+        /// <summary>
+        /// 租户
+        /// </summary>
+        string Tenant { get; set; }
+
+        /// <summary>
+        /// 应用程序
+        /// </summary>
+        string Application { get; set; }
+
+        /// <summary>
+        /// 模块
+        /// </summary>
+        string Module { get; set; }
+
+        /// <summary>
+        /// 类名
+        /// </summary>
+        string Class { get; set; }
+
+        /// <summary>
+        /// 方法
+        /// </summary>
+        string Method { get; set; }
+
+        /// <summary>
+        /// 参数
+        /// </summary>
+        string Param { get; set; }
+
+        /// <summary>
+        /// 操作人编号
+        /// </summary>
+        string UserId { get; set; }
+
+        /// <summary>
+        /// 操作人
+        /// </summary>
+        string Operator { get; set; }
+
+        /// <summary>
+        /// 操作人角色
+        /// </summary>
+        string Role { get; set; }
+
+        /// <summary>
+        /// 标题
+        /// </summary>
+        string Caption { get; set; }
+
+        /// <summary>
+        /// 内容
+        /// </summary>
+        string Content { get; set; }
+
+        /// <summary>
+        /// Sql语句
+        /// </summary>
+        string Sql { get; set; }
+
+        /// <summary>
+        /// Sql参数
+        /// </summary>
+        string SqlParams { get; set; }
+
+        /// <summary>
+        /// 错误码
+        /// </summary>
+        string ErrorCode { get; set; }
+
+        /// <summary>
+        /// 异常信息
+        /// </summary>
+        string Exception { get; set; }
+
+        /// <summary>
+        /// 排序
+        /// </summary>
+        int Order { get; set; }
+
+        /// <summary>
+        /// 文件名
+        /// </summary>
+        string FileName { get; set; }
+
+        /// <summary>
+        /// 文件路径
+        /// </summary>
+        string FilePath { get; set; }
+    }
+}
---
+++ tests/Bing.LogDashboard.Test/LogContentTest.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using Bing.LogDashboard.Models;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Bing.LogDashboard.Test
+{
+    public class LogContentTest:TestBase
+    {
+        /// <summary>
+        /// 文件路径
+        /// </summary>
+        private readonly string _filePath = @"D:\test\Utopa.Tms\Trace\2019-06-28\Trace.0.log";
+
+        /// <summary>
+        /// 分隔符
+        /// </summary>
+        public const string Separetor =
+            "-----------------------------------------------------------------------------------------------------------------------------\n\n";
+
+        public LogContentTest(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        
+
+        [Fact]
+        public void Test_FileContentSplit()
+        {
+            var text = File.ReadAllText(_filePath);
+            var contents = text.Split(Separetor);
+            var logContents = new List<LogContent>();
+            foreach (var content in contents)
+            {
+                if (string.IsNullOrWhiteSpace(content))
+                {
+                    continue;
+                }
+                var lines = content.Split('\n');
+                var logContent = new LogContent();
+                var sb = new StringBuilder();
+                for (var i = 0; i < lines.Length; i++)
+                {
+                    // 添加当前行数据
+                    sb.AppendLine(lines[i]);
+                    // 如果有下一行，则进行判断下一行是否多行文本
+                    if (i + 1 < lines.Length)
+                    {
+                        // 如果是多行文本，则不做任何操作
+                        // 如果是单行文本，则清空数据，并赋值
+                        if (!IsMultiLineText(lines[i + 1]))
+                        {
+                            logContent.Content = sb.ToString();
+                            sb.Clear();
+                        }
+                    }
+                    else if(i+1==lines.Length)
+                    {
+                        logContent.Content = sb.ToString();
+                        sb.Clear();
+                    }
+                }
+                logContents.Add(logContent);
+            }
+        }
+
+        /// <summary>
+        /// 是否多行文本
+        /// </summary>
+        /// <param name="content">内容</param>
+        private bool IsMultiLineText(string content)
+        {
+            return !Regex.IsMatch(content, @"^\d{1,2}\.\s");
+        }
+
+        /// <summary>
+        /// 是否目标行
+        /// </summary>
+        /// <param name="key">键名</param>
+        /// <param name="content">内容</param>
+        private bool IsTargetLine(string key, string content)
+        {
+            return Regex.IsMatch(content, @"^\d{1,2}\.\s" + key);
+        }
+
+        /// <summary>
+        /// 获取操作时间
+        /// </summary>
+        /// <param name="content">内容</param>
+        private string GetOperationTime(string content)
+        {
+            var match = Regex.Match(content, @"操作时间:\s\d{4}-\d{1,2}-\d{1,2}\s\d{1,2}:\d{1,2}:\d{1,2}.\d{1,3}");
+            if (match.Success)
+            {
+                return match.Value.Replace("操作时间", "");
+            }
+
+            return string.Empty;
+        }
+
+        private string GetKey(string content)
+        {
+            var match = Regex.Match(content, @"^\d{1,2}\. (([\u4e00-\u9fa5]|[A-Za-z]){0,})");
+            if (match.Success)
+            {
+                return Regex.Replace(match.Value, @"^\d{1,2}\.\s", "");
+            }
+
+            return string.Empty;
+        }
+    }
+}
